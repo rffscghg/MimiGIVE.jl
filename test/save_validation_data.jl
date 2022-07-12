80d1b6caef00d71d981fa64a67378a653c2a7854
lisarennels <lrennels@berkeley.edu>
2022-07-11 20:40:55 -0700

    Finish regression testing and validation files

--- test/save_validation_data.jl
+++ test/save_validation_data.jl
@@ -1,10 +1,12 @@
 using MimiGIVE
+using Random
+
 include("utils.jl")
 
 # This script saves a set of validation data for the give date in the validation_data
 # folder.  These data are used for regression testing by test_regression.jl.
 
-curr_date = "07_07_2022"
+curr_date = "07_11_2022"
 
 ##------------------------------------------------------------------------------
 ## Model Data
@@ -55,21 +57,69 @@ outdir = joinpath(@__DIR__, "validation_data", "validation_data_$curr_date", "de
 isdir(outdir) || mkpath(outdir)
 m = MimiGIVE.get_model()
 
-save_scc_data(outdir; m = m, year = 2020, discount_rates = dice_discount_rates, gas = :CO2)
-save_scc_data(outdir; m = m, year = 2020, discount_rates = dice_discount_rates, gas = :CH4)
-save_scc_data(outdir; m = m, year = 2020, discount_rates = dice_discount_rates, gas = :N2O)
+save_scc_data(outdir; m = m, year = 2020, discount_rates = discount_rates, gas = :CO2)
+save_scc_data(outdir; m = m, year = 2020, discount_rates = discount_rates, gas = :CH4)
+save_scc_data(outdir; m = m, year = 2020, discount_rates = discount_rates, gas = :N2O)
 
 # SSP245, SC-CO2 and SC-CH4 and SC-N2O in year 2020
 outdir = joinpath(@__DIR__, "validation_data", "validation_data_$curr_date", "SSP245_model_SCC_2020")
 isdir(outdir) || mkpath(outdir)
 m = MimiGIVE.get_model(; socioeconomics_source = :SSP, SSP_scenario = "SSP245")
 
-save_scc_data(outdir; m = m, year = 2020, discount_rates = dice_discount_rates, gas = :CO2)
-save_scc_data(outdir; m = m, year = 2020, discount_rates = dice_discount_rates, gas = :CH4)
-save_scc_data(outdir; m = m, year = 2020, discount_rates = dice_discount_rates, gas = :N2O)
+save_scc_data(outdir; m = m, year = 2020, discount_rates = discount_rates, gas = :CO2)
+save_scc_data(outdir; m = m, year = 2020, discount_rates = discount_rates, gas = :CH4)
+save_scc_data(outdir; m = m, year = 2020, discount_rates = discount_rates, gas = :N2O)
 
 ##------------------------------------------------------------------------------
 ## Compute SCC MCS Data
 ##------------------------------------------------------------------------------
 
-# TODO
+discount_rates = [
+                    # Constant discount rates
+                    (label = "CR 1%", prtp = 0.01, eta = 0.0), (label = "CR 2%", prtp = 0.02, eta = 0.0), (label = "CR 2.5%", prtp = 0.025, eta = 0.0), (label = "CR 3%", prtp = 0.03, eta = 0.0), (label = "CR 5%", prtp = 0.05, eta = 0.0),
+                    # Ramsey discount rates calibrated a la NPP
+                    (label = "1.5%", prtp = exp(9.149606e-05) - 1, eta = 1.016010e+00), (label = "2.0%", prtp = exp(0.001972641) - 1, eta = 1.244458999), (label = "2.5%", prtp = exp(0.004618784) - 1, eta = 1.421158088), (label = "3.0%", prtp = exp(0.007702711) - 1, eta = 1.567899391),
+                    # Other Ramsey discount rates
+                    (label = "DICE2016", prtp = 0.015, eta = 1.45)
+                ]
+
+save_list = [
+    (:DamageAggregator, :total_damage),
+    (:DamageAggregator, :total_damage_share),
+    (:DamageAggregator, :total_damage_domestic),
+    (:DamageAggregator, :cromar_mortality_damage),
+    (:DamageAggregator, :agriculture_damage),
+    (:DamageAggregator, :energy_damage),
+    (:DamageAggregator, :cromar_mortality_damage_domestic),
+    (:DamageAggregator, :agriculture_damage_domestic),
+    (:DamageAggregator, :energy_damage_domestic),
+    (:global_netconsumption, :net_consumption),
+    (:global_netconsumption, :net_cpc),
+    (:global_netconsumption, :global_gdp),
+    (:global_netconsumption, :global_population),   
+]
+
+n = 3
+seed = 999
+
+# default model, SC-CO2 and SC-CH4 and SC-N2O in year 2020
+for gas in [:CO2, :CH4, :N2O]
+    outdir = joinpath(@__DIR__, "validation_data", "validation_data_$curr_date", "default_model_MCS_SCC_2020", "$gas")
+    isdir(outdir) || mkpath(outdir)
+    m = MimiGIVE.get_model()
+    save_scc_mcs_data(seed, outdir, n; 
+                    m = m, year = 2020, discount_rates = discount_rates, gas = gas,
+                    save_list = save_list, save_md = true, save_cpc = true, save_slr_damages = true,
+                    compute_sectoral_values = true, compute_domestic_values = true)
+end
+
+# SSP245, SC-CO2 and SC-CH4 and SC-N2O in year 2020
+for gas in [:CO2, :CH4, :N2O]
+    outdir = joinpath(@__DIR__, "validation_data", "validation_data_$curr_date", "SSP245_model_MCS_SCC_2020", "$gas")
+    isdir(outdir) || mkpath(outdir)
+    m = MimiGIVE.get_model(; socioeconomics_source = :SSP, SSP_scenario = "SSP245")
+    save_scc_mcs_data(seed, outdir, n; 
+                    m = m, year = 2020, discount_rates = discount_rates, gas = gas,
+                    save_list = save_list, save_md = true, save_cpc = true, save_slr_damages = true,
+                    compute_sectoral_values = true, compute_domestic_values = true)
+end
--- test/test_regression.jl
+++ test/test_regression.jl
@@ -4,10 +4,10 @@ using MimiGIVE
 include("utils.jl")
 
 # date of validation data
-validation_date = "07_07_2022"
+validation_date = "07_11_2022"
 
 ##------------------------------------------------------------------------------
-## Model Data
+## Validate Model Data
 ##------------------------------------------------------------------------------
 
 savevars = [
@@ -37,7 +37,7 @@ m = MimiGIVE.get_model(; socioeconomics_source = :SSP, SSP_scenario = "SSP245")
 validate_model_data(m, savevars, validationdir)
 
 ##------------------------------------------------------------------------------
-## Compute SCC Data
+## Validate SCC Data
 ##------------------------------------------------------------------------------
 discount_rates = [
                     # Constant discount rates
@@ -48,18 +48,83 @@ discount_rates = [
                     (label = "DICE2016", prtp = 0.015, eta = 1.45)
                 ]
 
+for gas in [:CO2, :N2O, :CH4]
+    # default model, SC-CO2 and SC-CH4 and SC-N2O in year 2020
+    validationdir = joinpath(@__DIR__, "validation_data", "validation_data_$validation_date", "default_model_SCC_2020")
+    m = MimiGIVE.get_model()
+    validate_scc_data(validationdir; m = m, year = 2020, discount_rates = discount_rates, gas = gas)
+
+    # SSP245 model, SC-CO2 and SC-CH4 and SC-N2O in year 2020
+    validationdir = joinpath(@__DIR__, "validation_data","validation_data_$validation_date", "SSP245_model_SCC_2020")
+    m = MimiGIVE.get_model(; socioeconomics_source = :SSP, SSP_scenario = "SSP245")
+    validate_scc_data(validationdir; m = m, year = 2020, discount_rates = discount_rates, gas = gas)
+end
+
+##------------------------------------------------------------------------------
+## Validate SCC MCS Data
+##------------------------------------------------------------------------------
+discount_rates = [
+                    # Constant discount rates
+                    (label = "CR 1%", prtp = 0.01, eta = 0.0), (label = "CR 2%", prtp = 0.02, eta = 0.0), (label = "CR 2.5%", prtp = 0.025, eta = 0.0), (label = "CR 3%", prtp = 0.03, eta = 0.0), (label = "CR 5%", prtp = 0.05, eta = 0.0),
+                    # Ramsey discount rates calibrated a la NPP
+                    (label = "1.5%", prtp = exp(9.149606e-05) - 1, eta = 1.016010e+00), (label = "2.0%", prtp = exp(0.001972641) - 1, eta = 1.244458999), (label = "2.5%", prtp = exp(0.004618784) - 1, eta = 1.421158088), (label = "3.0%", prtp = exp(0.007702711) - 1, eta = 1.567899391),
+                    # Other Ramsey discount rates
+                    (label = "DICE2016", prtp = 0.015, eta = 1.45)
+                ]
+
+save_list = [
+    (:DamageAggregator, :total_damage),
+    (:DamageAggregator, :total_damage_share),
+    (:DamageAggregator, :total_damage_domestic),
+    (:DamageAggregator, :cromar_mortality_damage),
+    (:DamageAggregator, :agriculture_damage),
+    (:DamageAggregator, :energy_damage),
+    (:DamageAggregator, :cromar_mortality_damage_domestic),
+    (:DamageAggregator, :agriculture_damage_domestic),
+    (:DamageAggregator, :energy_damage_domestic),
+    (:global_netconsumption, :net_consumption),
+    (:global_netconsumption, :net_cpc),
+    (:global_netconsumption, :global_gdp),
+    (:global_netconsumption, :global_population),   
+]
+
+n = 3
+seed = 999
+
 # default model, SC-CO2 and SC-CH4 and SC-N2O in year 2020
-validationdir = joinpath(@__DIR__, "validation_data", "validation_data_$validation_date", "default_model_SCC_2020")
-m = MimiGIVE.get_model()
-validate_scc_data(validationdir; m = m, year = 2020, discount_rates = discount_rates, gas = :CO2)
+for gas in [:CO2, :N2O, :CH4]
+    validationdir = joinpath(@__DIR__, "validation_data", "validation_data_$validation_date", "default_model_MCS_SCC_2020", "$gas")
+    m = MimiGIVE.get_model()
+    validate_scc_mcs_data(seed, validationdir, n;
+                            m = m, 
+                            year = 2020, 
+                            discount_rates = discount_rates,
+                            gas = gas,
+                            save_list = save_list,
+                            save_md = true,
+                            save_cpc = true,
+                            save_slr_damages = true,
+                            compute_sectoral_values = true,
+                            compute_domestic_values = true,
+                        )
+end
 
 # SSP245 model, SC-CO2 and SC-CH4 and SC-N2O in year 2020
-validationdir = joinpath(@__DIR__, "validation_data","validation_data_$validation_date", "SSP245_model_SCC_2020")
-m = MimiGIVE.get_model(; socioeconomics_source = :SSP, SSP_scenario = "SSP245")
-validate_scc_data(validationdir; m = m, year = 2020, discount_rates = discount_rates, gas = :CO2)
-
-##------------------------------------------------------------------------------
-## Compute SCC MCS Data
-##------------------------------------------------------------------------------
+for gas in [:CO2, :N2O, :CH4]
+    validationdir = joinpath(@__DIR__, "validation_data", "validation_data_$validation_date", "SSP245_model_MCS_SCC_2020", "$gas")
+    m = MimiGIVE.get_model(; socioeconomics_source = :SSP, SSP_scenario = "SSP245")
+    validate_scc_mcs_data(seed, validationdir, n;
+                            m = m,
+                            year = 2020, 
+                            discount_rates = discount_rates,
+                            gas = gas,
+                            save_list = save_list,
+                            save_md = true,
+                            save_cpc = true,
+                            save_slr_damages = true,
+                            compute_sectoral_values = true,
+                            compute_domestic_values = true,
+                        )
+end
 
 end # module
