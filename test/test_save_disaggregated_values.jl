8062bd96a5442397d131b25d3021fc70bb9e8bcd
lisarennels <lrennels@berkeley.edu>
2024-04-05 20:23:32 -0700

    Add more tests for damages

--- test/test_save_disaggregated_values.jl
+++ test/test_save_disaggregated_values.jl
@@ -5,11 +5,11 @@ using Test
 using Query
 using DataFrames
 using CSVFiles
+using Mimi
 
 atol = 1e-9
 
 output_dir = mktempdir()
-# output_dir = "/Users/lisarennels/.julia/dev/MimiGIVE/test/tmp"
 n = 3
 m = MimiGIVE.get_model()
 discount_rates = [(label="Ramsey", prtp=0.015, eta=1.45)]
@@ -22,15 +22,44 @@ results = MimiGIVE.compute_scc(m,
                                 compute_sectoral_values = true,
                                 compute_disaggregated_values = true,
                                 compute_domestic_values = true,
-                                n = n
+                                n = n,
+                                save_list = [(:Socioeconomic, :population), 
+                                            (:Socioeconomic, :gdp)]
                             )
 
-# test damages 
-
-# test socioeconomics
+##
+## Sectoral Damages
+##
 
 ##
-## Test Marginal Damages 
+## Socioeconomics
+##
+
+# country_test = ["ABW", "CHI", "ZAF"] # random countries to test
+gdp_savelist = load(joinpath(output_dir, "results", "model_1", "Socioeconomic_gdp.csv")) |> 
+                DataFrame |>
+                @filter(_.time >= 2020) |>
+                DataFrame
+
+pop_savelist = load(joinpath(output_dir, "results", "model_1", "Socioeconomic_population.csv")) |> 
+                DataFrame |>
+                @filter(_.time >= 2020) |>
+                DataFrame 
+
+df_savelist = innerjoin(gdp_savelist, pop_savelist, on = [:time, :country, :trialnum])
+insertcols!(df_savelist, :gdppc => df_savelist.gdp ./ df_savelist.population .* 1e3)
+sort!(df_savelist, [:trialnum, :country, :time])
+
+for country in Mimi.dim_keys(m, :country)
+    disagg_values = load(joinpath(output_dir, "results", "disaggregated_values", "socioeconomics_country", "$country.csv")) |> DataFrame
+    savelist_values = df_savelist |> @filter(_.country == country) |> DataFrame
+
+    @test disagg_values.population ≈ savelist_values.population atol = atol
+    @test disagg_values.pc_gdp ≈ savelist_values.gdppc atol = 1e-6
+end
+
+##
+## Marginal Damages 
 ## 
 
 # compare domestic agriculture (FUND region) saved via original methods to the 
