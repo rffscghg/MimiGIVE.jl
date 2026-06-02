7458bd0951f35e201d1fdef73c092e324fe0d40b
David Anthoff <anthoff@berkeley.edu>
2026-06-02 12:20:46 -0700

    Try to fix data deps handling

---
+++ test/github_job_prep.jl
@@ -0,0 +1,11 @@
+using DataDeps
+
+register(DataDep(
+    "rffsps_v5",
+    "RFF SPs version v5",
+    "https://zenodo.org/record/6016583/files/rffsps_v5.7z",
+    "a39b51d7552d198123b1863ea5a131533715a7a7c9ff6fad4b493594ece49497",
+    post_fetch_method=unpack
+))
+
+datadep"rffsps_v5"
